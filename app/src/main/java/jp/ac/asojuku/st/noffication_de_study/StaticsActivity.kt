aced1e2f4a0523f61fde59912c0d554566c52409
NakayamaReiya <41409711+NakayamaReiya@users.noreply.github.com>
2019-05-27 22:22:49 +0900

    Add Activitys

--- app/src/main/java/jp/ac/asojuku/st/noffication_de_study/StaticsActivity.kt
+++ app/src/main/java/jp/ac/asojuku/st/noffication_de_study/StaticsActivity.kt
@@ -3,10 +3,37 @@ package jp.ac.asojuku.st.noffication_de_study
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
+//統計情報画面
 class StaticsActivity : AppCompatActivity() {
+
+    //定数
+    val user_id:Int = 12345678 //ユーザID
+    val my_record //TODO ここから
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_statics)
     }
+
+    //my記録表示
+    fun printMyRecord() {
+
+    }
+
+    //問題正答率表示
+    fun printCorrectRate() {
+
+    }
+
+    //表示切り替え
+    fun changeTab() {
+
+    }
+
+    //表示文字列生成
+    fun createStrToPrint() {
+
+    }
+
 }
