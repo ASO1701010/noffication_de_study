bc55036e38d492f39135f24fdca69cb731d29318
ryunosuke <ryuryuryu713@gmail.com>
2019-05-29 12:15:52 +0900

    1st

---
+++ app/src/main/java/jp/ac/asojuku/st/noffication_de_study/QuestionOptionActivity.kt
@@ -0,0 +1,37 @@
+package jp.ac.asojuku.st.noffication_de_study
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class QuestionOptionActivity : AppCompatActivity() {
+    var user_id: Int? = null
+    var question_list? = null
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_question_option)
+
+        loadChoise()
+    }
+
+    fun loadChoise(){
+
+    }
+
+    fun setChoise(){
+
+    }
+
+    fun decideSetting(){
+
+    }
+
+    fun discChoise(){
+
+    }
+
+    fun decideQuestion(){
+
+    }
+}
