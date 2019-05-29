b4e1fb9a5c621535a3a3f42c46fb79d1f431ef0c
ASO1701002 <1701002@st.asojuku.ac.jp>
2019-05-29 16:12:01 +0900

    TEST

---
+++ app/src/main/java/jp/ac/asojuku/st/noffication_de_study/OptionActivity.kt
@@ -0,0 +1,33 @@
+package jp.ac.asojuku.st.noffication_de_study
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+//TODO オプション画面:未完成(0%)
+class OptionActivity : AppCompatActivity() {
+
+    //TODO 定数の中身はすべて仮の値
+    val user_id = 12345678 //ユーザID
+    val user_auth = "USER_AUTH" //自動でログインする用の変数
+    val rule_num: Int = 123 //次に追加するruleのid
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_option)
+    }
+
+    //Googleログインを行うメソッド
+    fun login() {
+
+    }
+
+    //ルール登録
+    fun regRule() {
+
+    }
+
+    //ルール追加
+    fun addRule() {
+
+    }
+}
