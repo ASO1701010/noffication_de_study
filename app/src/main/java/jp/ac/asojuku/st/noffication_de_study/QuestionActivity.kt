ede60b6a7253d1e85ac8d6abe771459565227d0e
ASO1701001 <1701001@st.asojuku.ac.jp>
2019-06-04 11:39:40 +0900

    File Add

---
+++ app/src/main/java/jp/ac/asojuku/st/noffication_de_study/QuestionActivity.kt
@@ -0,0 +1,12 @@
+package jp.ac.asojuku.st.noffication_de_study
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class QuestionActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_question)
+    }
+}
+
