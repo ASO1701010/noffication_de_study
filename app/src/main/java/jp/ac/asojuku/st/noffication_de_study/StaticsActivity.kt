8f25a3bb0be63e3843c28b5aa142996e1b8f002a
ASO1701002 <1701002@st.asojuku.ac.jp>
2019-06-12 12:52:52 +0900

    ExamQuestionsOpenHelperの修正、MainActivityのバグの修正、StaticsActivityのAPIとの通信部実装

--- app/src/main/java/jp/ac/asojuku/st/noffication_de_study/StaticsActivity.kt
+++ app/src/main/java/jp/ac/asojuku/st/noffication_de_study/StaticsActivity.kt
@@ -2,8 +2,11 @@ package jp.ac.asojuku.st.noffication_de_study
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_statics.*
 import org.jetbrains.anko.startActivity
+import org.json.JSONObject
 
 //TODO 統計情報画面:未完成(0%)
 class StaticsActivity : AppCompatActivity() {
@@ -14,12 +17,14 @@ class StaticsActivity : AppCompatActivity() {
     var correct_rate:String = "999%" //正答率
     var text_print:String = "testTextPrint" //表示に利用する
 
+    var jsondata = JSONObject()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_statics)
 
         SA_Back_BTN.setOnClickListener {
-            startActivity<TitleActivity>()
+            finish()
         }
 
         SA_My_Statics_BTN.setOnClickListener {
@@ -27,7 +32,7 @@ class StaticsActivity : AppCompatActivity() {
         }
 
         SA_Question_Statics_BTN.setOnClickListener {
-
+            get_statics()
         }
     }
 
@@ -49,6 +54,22 @@ class StaticsActivity : AppCompatActivity() {
     //表示文字列生成
     fun createStrToPrint() {
 
+    }
+
+    //統計情報取得
+    fun get_statics(){
+        ApiGetTask {
+            if (!it.isNullOrEmpty()) {
+                Log.d("it",it)
+                var json = JSONObject(it)
+                if(json.getString("status") == "E00") {
+                    Toast.makeText(this, "おきのどくですが、とうけいのしょは、きえてしまいました(´・∀・｀)", Toast.LENGTH_SHORT).show()
+                }
+                jsondata = json
+            } else {
+                Toast.makeText(this, "APIの通信に失敗しました(´･ω･`)", Toast.LENGTH_SHORT).show()
+            }
+        }.execute("db-update.php")
     }
 
 }
