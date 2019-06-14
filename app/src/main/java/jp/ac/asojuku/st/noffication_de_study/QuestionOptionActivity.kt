b456de09ae1ada8c5aa9c64f9b23fb84891f03d0
ryunosuke <ryuryuryu713@gmail.com>
2019-06-14 10:24:30 +0900

    QuestionOptionActivityがおおよそ完成(多分)

--- app/src/main/java/jp/ac/asojuku/st/noffication_de_study/QuestionOptionActivity.kt
+++ app/src/main/java/jp/ac/asojuku/st/noffication_de_study/QuestionOptionActivity.kt
@@ -6,7 +6,8 @@ import jp.ac.asojuku.st.noffication_de_study.db.ExamsQuestionsOpenHelper
 import jp.ac.asojuku.st.noffication_de_study.db.QuestionsGenresOpenHelper
 import kotlinx.android.synthetic.main.activity_question_option.*
 import org.jetbrains.anko.startActivity
-import jp.ac.asojuku.st.noffication_de_study.R
+import java.util.*
+import kotlin.collections.ArrayList
 
 //TODO 問題オプション画面：未完成（0%）
 class QuestionOptionActivity : AppCompatActivity() {
@@ -75,56 +76,71 @@ class QuestionOptionActivity : AppCompatActivity() {
         var TempYear: ArrayList<ArrayList<Int>>?
         lateinit var TempYear_list: ArrayList<ArrayList<ArrayList<Int>>?>
 
+        // 出題年度が選択されなかった場合、「"empty"」のままになる
+        var ExamName = "empty"
+
         // 試験回選択
         // 出題IDはFEだけなので、exams_idは1?
         if (QOA_Select_Exam_Number_H31S_RBTN.isChecked) {
             TempYear = EQOH.find_all_questions(1, "FE2019S")
             TempYear_list.add(TempYear)
+            ExamName = "FE2019S"
         }
         if (QOA_Select_Exam_Number_H30F_RBTN.isChecked) {
             TempYear = EQOH.find_all_questions(1, "FE2018F")
             TempYear_list.add(TempYear)
+            ExamName = "FE2018F"
         }
         if (QOA_Select_Exam_Number_H30S_RBTN.isChecked) {
             TempYear = EQOH.find_all_questions(1, "FE2018S")
             TempYear_list.add(TempYear)
+            ExamName = "FE2018S"
         }
         if (QOA_Select_Exam_Number_H29F_RBTN.isChecked) {
             TempYear = EQOH.find_all_questions(1, "FE2017F")
             TempYear_list.add(TempYear)
+            ExamName = "FE2017F"
         }
         if (QOA_Select_Exam_Number_H29S_RBTN.isChecked) {
             TempYear = EQOH.find_all_questions(1, "FE2017S")
             TempYear_list.add(TempYear)
+            ExamName = "FE2017S"
         }
         if (QOA_Select_Exam_Number_H28F_RBTN.isChecked) {
             TempYear = EQOH.find_all_questions(1, "FE2016F")
             TempYear_list.add(TempYear)
+            ExamName = "FE2016F"
         }
 
-
+        // 出題年度が選択されなかった場合(ExamData)が「""」だった場合
+        if (ExamName == "empty") {
+            TempYear_list.add(EQOH.find_all_questions(1, "FE2019S"))
+            TempYear_list.add(EQOH.find_all_questions(1, "FE2018F"))
+            TempYear_list.add(EQOH.find_all_questions(1, "FE2018S"))
+            TempYear_list.add(EQOH.find_all_questions(1, "FE2017F"))
+            TempYear_list.add(EQOH.find_all_questions(1, "FE2017S"))
+            TempYear_list.add(EQOH.find_all_questions(1, "FE2016F"))
+        }
         // ジャンルの読み込み
         val GOH = QuestionsGenresOpenHelper(db)
         var genre1_Questions: ArrayList<Int>? = null
+        var genre2_Questions: ArrayList<Int>? = null
         // ジャンル検索
         if (QOA_Select_Genres_1.isChecked) {
             genre1_Questions = GOH.find_genre_questions(1)
         }
         if (QOA_Select_Genres_2.isChecked) {
-            val genre2_Questions = GOH.find_genre_questions(2)
+            genre2_Questions = GOH.find_genre_questions(2)
         }
 
         // 取得した問題から全てのArrayListに存在するものを書き出す
         // ArrayListのTempQuestionsに出題する問題を格納する
         var TempQuestions = ArrayList<Int>()
 
-//        TempQuestions = genre1_Questions.filter
-
         if (genre1_Questions != null) {
             for (ty in TempYear_list) {
                 for (i in 0..ty!!.size) {
                     for (j in 1..genre1_Questions.size) {
-
                         if (genre1_Questions.get(i) == ty.get(j).get(2)) {
                             TempQuestions.add(genre1_Questions.get(i))
                         }
@@ -133,13 +149,35 @@ class QuestionOptionActivity : AppCompatActivity() {
             }
         }
 
-        // TempQuestionsに保存した問題を次のページに受け渡す
+        if (genre2_Questions != null) {
+            for (ty in TempYear_list) {
+                for (i in 0..ty!!.size) {
+                    for (j in 1..genre2_Questions.size) {
+                        if (genre2_Questions.get(i) == ty.get(j).get(2)) {
+                            TempQuestions.add(genre2_Questions.get(i))
+                        }
+                    }
+                }
+            }
+        }
 
+        // ランダム出題するか、そうじゃないかで問題順を並び替える
+        if (randomBoolean == true) {
+            Collections.shuffle(TempQuestions)
+        }
+
+        // 問題数に応じて問題を選択する
+        var TempQuestions2 = ArrayList<Int>()
+        for (i in 0..SpinnerNum - 1) {
+            TempQuestions2.add(TempQuestions.get(i))
+        }
+
+
+        // TempQuestionsに保存した問題を次のページに受け渡す
+        val ED = ExamData(1, "FE", ExamName)
+        ED.set_list_data(TempQuestions2)
 
     }
-
-
-}
 
 //読み込んだ選択肢を各セレクトボックスやスピナーにセットする
 //object型を引数には設定できない
@@ -147,20 +185,21 @@ class QuestionOptionActivity : AppCompatActivity() {
 //
 //    }
 
-//設定完了時の処理。出題する問題を決定し配列に格納、画面遷移を行う
-fun decideSetting() {
+    //設定完了時の処理。出題する問題を決定し配列に格納、画面遷移を行う
+    fun decideSetting() {
 
-}
+    }
 
-//選択設定判定
+    //選択設定判定
 //設定されたチェックをすべて検索しsetting_listに登録
-fun discChoice() {
+    fun discChoice() {
 
-}
+    }
 
-//出題問題決定
+    //出題問題決定
 // 出題する問題を決定し問題IDを配列で返す。未設定時はランダムに20問
 //TODO このメソッドは戻り値にArray<Int>を戻り値にする？
-fun decideQuestion() {
+    fun decideQuestion() {
 
+    }
 }
