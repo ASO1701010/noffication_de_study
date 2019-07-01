d7ac640fcdda058c53ea928994dc93a1e8f04889
ASO1701002 <1701002@st.asojuku.ac.jp>
2019-07-01 11:29:44 +0900

    通知で4択出題ができるようになりました。

--- app/src/main/java/jp/ac/asojuku/st/noffication_de_study/OptionActivity.kt
+++ app/src/main/java/jp/ac/asojuku/st/noffication_de_study/OptionActivity.kt
@@ -1,18 +1,17 @@
 package jp.ac.asojuku.st.noffication_de_study
 
 import android.annotation.SuppressLint
-import android.app.*
+import android.app.TimePickerDialog
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
-import android.os.Build
 import android.os.Bundle
-import android.support.annotation.RequiresApi
-import android.support.v4.app.NotificationCompat
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
 import android.view.View
-import android.widget.*
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.Toast
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
@@ -21,13 +20,8 @@ import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.GoogleAuthProvider
-import jp.ac.asojuku.st.noffication_de_study.db.AnswersOpenHelper
-import jp.ac.asojuku.st.noffication_de_study.db.QuestionsOpenHelper
 import kotlinx.android.synthetic.main.activity_option.*
-import org.jetbrains.anko.custom.async
-import org.jetbrains.anko.doAsync
 import org.json.JSONObject
-import kotlin.random.Random
 
 class OptionActivity : AppCompatActivity() {
     // Google SignIn
@@ -40,15 +34,12 @@ class OptionActivity : AppCompatActivity() {
 
     private val noticeIntervalItems = arrayOf("5", "10", "15", "20", "25", "30")
 
-    @RequiresApi(Build.VERSION_CODES.O)
     @SuppressLint("CommitPrefEdits")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_option)
 
         //通知のテストデータ
-        showNotification(1)
-
         spEditor = getSharedPreferences("user_data", Context.MODE_PRIVATE).edit()
 
         val adapter = ArrayAdapter(applicationContext, android.R.layout.simple_spinner_item, noticeIntervalItems)
@@ -188,113 +179,5 @@ class OptionActivity : AppCompatActivity() {
                 }
             }.execute("add-user.php", hashMapOf("token" to user.uid).toString())
         }
-    }
-
-//    fun getRandomQuestion(): Int{
-//        val randomInt = Random.nextInt(10)
-//    }
-
-    @RequiresApi(Build.VERSION_CODES.O)
-    fun showNotification(question_id: Int) {
-        val db = SQLiteHelper(this).readableDatabase
-        val dbHelper = QuestionsOpenHelper(db)
-
-        var question_text = dbHelper.find_question(question_id)
-        if(question_text == null){
-            return
-        }
-        val mChannel = NotificationChannel("0","問題通知", NotificationManager.IMPORTANCE_HIGH)
-        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        notificationManager.createNotificationChannel(mChannel)
-        val contentView = RemoteViews(getPackageName(), R.layout.notifi_layout)
-
-
-        val intent1 = Intent(this, AnswerActivity::class.java)
-        val intent2 = Intent(this, AnswerActivity::class.java)
-        val intent3 = Intent(this, AnswerActivity::class.java)
-        val intent4 = Intent(this, AnswerActivity::class.java)
-        val intentQuestion = Intent(this, QuestionActivity::class.java)
-
-        // 通知ボタンタップ時に渡す値
-        intent1.putExtra("question_id",question_id)
-        intent2.putExtra("question_id",question_id)
-        intent3.putExtra("question_id",question_id)
-        intent4.putExtra("question_id",question_id)
-
-        val data1 = ExamData(1,"","")
-        data1.set_list_data(arrayListOf(question_id))
-        val data2 = ExamData(1,"","")
-        data2.set_list_data(arrayListOf(question_id))
-        val data3 = ExamData(1,"","")
-        data3.set_list_data(arrayListOf(question_id))
-        val data4 = ExamData(1,"","")
-        data4.set_list_data(arrayListOf(question_id))
-        val data5 = ExamData(1,"","")
-        data5.set_list_data(arrayListOf(question_id))
-
-        data1.question_current = question_id
-        data2.question_current = question_id
-        data3.question_current = question_id
-        data4.question_current = question_id
-        data5.question_current = question_id
-        data1.question_next = question_id
-        data2.question_next = question_id
-        data3.question_next = question_id
-        data4.question_next = question_id
-        data5.question_next = question_id
-
-        regAnswer(0,data1)
-        regAnswer(1,data2)
-        regAnswer(2,data3)
-        regAnswer(3,data4)
-
-
-        intent1.putExtra("exam_data",data1)
-        intent2.putExtra("exam_data",data2)
-        intent3.putExtra("exam_data",data3)
-        intent4.putExtra("exam_data",data4)
-        intentQuestion.putExtra("exam_data",data5)
-
-
-
-        // 各intentをPendingIntentに変換
-        val pi1 = PendingIntent.getActivity(this, 1983418741, intent1, PendingIntent.FLAG_UPDATE_CURRENT)
-        contentView.setOnClickPendingIntent(R.id.notifyButton, pi1)
-        val pi2 = PendingIntent.getActivity(this, 1983418742, intent2, PendingIntent.FLAG_UPDATE_CURRENT)
-        contentView.setOnClickPendingIntent(R.id.notifyButton2, pi2)
-        val pi3 = PendingIntent.getActivity(this, 1983418743, intent3, PendingIntent.FLAG_UPDATE_CURRENT)
-        contentView.setOnClickPendingIntent(R.id.notifyButton3, pi3)
-        val pi4 = PendingIntent.getActivity(this, 1983418744, intent4, PendingIntent.FLAG_UPDATE_CURRENT)
-        contentView.setOnClickPendingIntent(R.id.notifyButton4, pi4)
-
-        val piQuestion = PendingIntent.getActivity(this,1983418745, intentQuestion, PendingIntent.FLAG_CANCEL_CURRENT)
-
-        // 表示するテキスト
-        contentView.setTextViewText(R.id.notifiText,question_text.get(1))
-
-        // 表示内容を生成
-        val notification = NotificationCompat.Builder(this,"0")
-            .setCustomBigContentView(contentView)
-            .setContentTitle("問題ですが何か？？")
-            .setContentIntent(piQuestion)
-            .setSmallIcon(R.drawable.abc_ic_star_half_black_16dp)
-            .build()
-
-        // 表示
-        notificationManager.notify(0,notification)
-    }
-    fun regAnswer(choice_number: Int, examData: ExamData) {
-        //自分の解答を登録
-        examData.answered_list.add(choice_number)
-        //正解をDBから取得
-        val answers = SQLiteHelper(this)
-        val db = answers.readableDatabase
-        val AOH = AnswersOpenHelper(db)
-        var answer = AOH.find_answers(examData.question_current)?.get(1) //正しい正解
-        var isCorrected = false //正解だった場合にtrueにする
-        if (choice_number == answer) {
-            isCorrected = true
-        }
-        examData.isCorrect_list.add(isCorrected)//解いた問題が正解だったかどうかがBoolean型で入る
     }
 }
